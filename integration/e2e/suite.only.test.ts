685e5bfb81d388ffcce25441106ea33d76474603
Taisia Pitko <taisia.pitko@greenlight.guru>
2025-01-10 00:42:33 +0100

    fix tests

--- integration/e2e/suite.only.test.ts
+++ integration/e2e/suite.only.test.ts
@@ -1,5 +1,5 @@
 describe.only('suite only', () => {
   it('test', { tags: ['@tag'] }, function () {
-    expect(this.test?.tags).deep.eq([{ tag: '@tag', info: [] }]);
+    expect(this.test?.tags).deep.eq([{ tag: '@tag', info: [], isOwnTag: true }]);
   });
 });
