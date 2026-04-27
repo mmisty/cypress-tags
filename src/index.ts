764e4e45d3a087922e70ce2ea41780cf26cee0e3
Taisia Pitko <taisia.pitko@greenlight.guru>
2026-04-27 11:37:25 +0200

    fix types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="./cypress/cypress.ts" />
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="./mocha/index.ts" />
-// here export only functions to run in browser
-
-// usage: import '@mmisty/cypress-tags';
+import './cypress/cypress';
+import './mocha';
 import { registerTags } from './setup';
 
 registerTags();
